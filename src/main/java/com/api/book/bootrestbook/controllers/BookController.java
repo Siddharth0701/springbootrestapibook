eff23ee7ae19cf949bf84a2f43e071a64a9ae54d
Siddharth0701 <siddharth.singh0701@gmail.com>
2023-01-06 01:36:55 +0530

    added http request status using responceEntity

--- src/main/java/com/api/book/bootrestbook/controllers/BookController.java
+++ src/main/java/com/api/book/bootrestbook/controllers/BookController.java
@@ -1,7 +1,12 @@
 package com.api.book.bootrestbook.controllers;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,43 +21,48 @@ import com.api.book.bootrestbook.services.BookService;
 public class BookController {
     @Autowired
     private BookService bookService;
-    // @RequestMapping(value = "/books", method = RequestMethod.GET)
-    //get all book
-    @GetMapping("/books")
-    public List<Book> getBooks() {
-       // Book book = new Book(1,"Java Programming","Siddharth Singh");
-      // Book book = new Book();
-        //  book.setId(11110);
-        //  book.setTitle("Java Complete Reference");
-        //  book.setAuthor("Siddharth Singh");
 
-        return this.bookService.getAllBooks();
+    // @RequestMapping(value = "/books", method = RequestMethod.GET)
+    // get all book
+    @GetMapping("/books")
+    public ResponseEntity<List<Book>> getBooks() {
+        // return this.bookService.getAllBooks();
+        List<Book> list = this.bookService.getAllBooks();
+        if (list.size() <= 0) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+
+        return ResponseEntity.of(Optional.of(list));
+
     }
-    //get book by id
+
+    // get book by id
     @GetMapping("/books/{id}")
-    public Book getBook(@PathVariable("id") int id){
+    public Book getBook(@PathVariable("id") int id) {
         return this.bookService.getBookById(id);
     }
-    //add book by post method
-@PostMapping("/books")
-    public Book addBook( @RequestBody Book book){
-        Book b= this.bookService.addBook(book);
+
+    // add book by post method
+    @PostMapping("/books")
+    public Book addBook(@RequestBody Book book) {
+        Book b = this.bookService.addBook(book);
         System.out.println(book);
-         return b;
+        return b;
 
     }
+
     // delete book handeler
     @DeleteMapping("/books/{bookId}")
-    public void deleteBook(@PathVariable("bookId") int bookId){
+    public void deleteBook(@PathVariable("bookId") int bookId) {
         this.bookService.deleteBook(bookId);
-        
-    }
-    //update book handler
-    @PutMapping("/books/{bookId}")
-    public Book updateBook(@RequestBody Book book ,@PathVariable("bookId") int bookId ){
-        this.bookService.updateBook(book,bookId);
-        return book;
 
+    }
+
+    // update book handler
+    @PutMapping("/books/{bookId}")
+    public Book updateBook(@RequestBody Book book, @PathVariable("bookId") int bookId) {
+        this.bookService.updateBook(book, bookId);
+        return book;
 
     }
 }
